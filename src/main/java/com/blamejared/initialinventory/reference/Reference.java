cb430cbc161a2d0fc985ea47b2ed1dd4aa101c60
Jared <jaredlll08@gmail.com>
2016-11-04 21:25:09 +0200

    Loads after enchiridion

--- src/main/java/com/blamejared/initialinventory/reference/Reference.java
+++ src/main/java/com/blamejared/initialinventory/reference/Reference.java
@@ -10,7 +10,7 @@ public class Reference {
     public static final String MOD_ID = "initialinventory";
     public static final String MOD_NAME = "InitialInventory";
     public static final String VERSION = "1.0.0";
-    public static final String DEPENDENCIES = "required-after:MineTweaker3;required-after:mtlib;";
+    public static final String DEPENDENCIES = "required-after:MineTweaker3;required-after:mtlib;after:enchiridion";
 
 
     public static File scriptFile;
