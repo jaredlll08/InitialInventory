2c235434eb2e763630b4451765e6386abc545507
Jared <jaredlll08@gmail.com>
2016-12-02 23:10:40 +0200

    Version push

--- src/main/java/com/blamejared/initialinventory/reference/Reference.java
+++ src/main/java/com/blamejared/initialinventory/reference/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
 
     public static final String MOD_ID = "initialinventory";
     public static final String MOD_NAME = "InitialInventory";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String DEPENDENCIES = "required-after:MineTweaker3;required-after:mtlib;after:enchiridion";
 
 
