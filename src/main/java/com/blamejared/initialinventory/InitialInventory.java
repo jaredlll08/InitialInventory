6ada231f2add8ff9518b4b4328ad1caa5decedd1
Jared <jaredlll08@gmail.com>
2020-04-18 00:02:15 +0200

    port to 1.14

--- src/main/java/com/blamejared/initialinventory/InitialInventory.java
+++ src/main/java/com/blamejared/initialinventory/InitialInventory.java
@@ -1,29 +1,46 @@
 package com.blamejared.initialinventory;
 
-import com.blamejared.initialinventory.events.CommonEventHandler;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.items.ItemHandlerHelper;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.*;
 
-import static com.blamejared.initialinventory.reference.Reference.*;
-
-@Mod(modid = MOD_ID, name = MOD_NAME, version = VERSION, dependencies = DEPENDENCIES)
+@Mod("initialinventory")
 public class InitialInventory {
     
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent e) {
+    public static final Map<String, List<Pair<ItemStack, Integer>>> STACK_MAP = new HashMap<>();
+    
+    public InitialInventory() {
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
+    @SubscribeEvent
+    public void playerLogin(final PlayerEvent.PlayerLoggedInEvent e) {
+        CompoundNBT tag = e.getPlayer().getPersistentData();
+        STACK_MAP.forEach((s, pairs) -> {
+            if(tag.getBoolean("initialinventory_" + s)) {
+                return;
+            }
+            pairs.forEach(pair -> {
+                if(pair.getRight() <= -1) {
+                    ItemHandlerHelper.giveItemToPlayer(e.getPlayer(), pair.getLeft().copy());
+                } else {
+                    if(e.getPlayer().inventory.getStackInSlot(pair.getRight()) != ItemStack.EMPTY) {
+                        ItemHandlerHelper.giveItemToPlayer(e.getPlayer(), pair.getLeft().copy());
+                    } else {
+                        e.getPlayer().inventory.setInventorySlotContents(pair.getRight(), pair.getLeft().copy());
+                    }
+                }
+            });
+            tag.putBoolean("initialinventory_" + s, true);
+        });
     }
     
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(new CommonEventHandler());
-    }
 }
---
+++ src/main/java/com/blamejared/initialinventory/InvHandler.java
@@ -0,0 +1,63 @@
+package com.blamejared.initialinventory;
+
+
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
+import com.blamejared.crafttweaker.api.actions.IUndoableAction;
+import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.logger.ILogger;
+import com.blamejared.crafttweaker.impl.item.MCItemStackMutable;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.LogicalSide;
+import org.apache.commons.lang3.tuple.Pair;
+import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.*;
+
+import static org.apache.logging.log4j.core.appender.rewrite.MapRewritePolicy.Mode.Add;
+
+@ZenCodeType.Name("mods.initialinventory.InvHandler")
+@ZenRegister
+public class InvHandler {
+    
+    @ZenCodeType.Method
+    public static void addStartingItem(String key, IItemStack item, @ZenCodeType.OptionalInt(-1) int slotId) {
+        CraftTweakerAPI.apply(new AddItem(key, item.getInternal(), slotId));
+    }
+    
+    private static class AddItem implements IUndoableAction {
+        
+        private final String key;
+        private final ItemStack stack;
+        private final int index;
+        
+        public AddItem(String key, ItemStack stack, int index) {
+            this.key = key;
+            this.stack = stack;
+            this.index = index;
+        }
+        
+        @Override
+        public void apply() {
+            List<Pair<ItemStack, Integer>> list = InitialInventory.STACK_MAP.computeIfAbsent(key, s -> new ArrayList<>());
+            list.add(Pair.of(stack, index));
+        }
+        
+        @Override
+        public String describe() {
+            return String.format("Adding starting item: %s in slot: %s in key: %s", new MCItemStackMutable(stack).getCommandString(), index, key);
+        }
+        
+        @Override
+        public void undo() {
+            List<Pair<ItemStack, Integer>> list = InitialInventory.STACK_MAP.computeIfAbsent(key, s -> new ArrayList<>());
+            list.removeIf(itemStackIntegerPair -> new MCItemStackMutable(itemStackIntegerPair.getKey()).matches(new MCItemStackMutable(stack)));
+        }
+        
+        @Override
+        public String describeUndo() {
+            return String.format("Removing starting item: %s in slot: %s in key: %s", new MCItemStackMutable(stack).getCommandString(), index, key);
+        }
+        
+    }
+}
