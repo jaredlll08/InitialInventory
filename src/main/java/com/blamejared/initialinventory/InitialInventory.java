ef9d0e55cc56e53e86d30e359056d1a38a4af2bb
Jared <jaredlll08@gmail.com>
2020-05-30 23:42:39 +0200

    Fix giving multiple items close #8

--- src/main/java/com/blamejared/initialinventory/InitialInventory.java
+++ src/main/java/com/blamejared/initialinventory/InitialInventory.java
@@ -1,6 +1,7 @@
 package com.blamejared.initialinventory;
 
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.MinecraftForge;
@@ -23,9 +24,17 @@ public class InitialInventory {
     
     @SubscribeEvent
     public void playerLogin(final PlayerEvent.PlayerLoggedInEvent e) {
-        CompoundNBT tag = e.getPlayer().getPersistentData();
+        CompoundNBT oldTag = e.getPlayer().getPersistentData();
+        CompoundNBT tag = e.getPlayer().getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
+        for(String name : oldTag.keySet()) {
+            if(name.startsWith("initialinventory_") && !tag.contains(name)){
+                tag.putBoolean(name, oldTag.getBoolean(name));
+            }
+        }
+        
         STACK_MAP.forEach((s, pairs) -> {
             if(tag.getBoolean("initialinventory_" + s)) {
+                System.out.println(s);
                 return;
             }
             pairs.forEach(pair -> {
@@ -41,6 +50,7 @@ public class InitialInventory {
             });
             tag.putBoolean("initialinventory_" + s, true);
         });
+        e.getPlayer().getPersistentData().put(PlayerEntity.PERSISTED_NBT_TAG, tag);
     }
     
 }
