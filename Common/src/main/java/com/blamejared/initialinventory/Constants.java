5be071234abd2cff06bf89bb6855694052fc3f93
Jared <jaredlll08@gmail.com>
2022-04-18 02:54:39 +0200

    Code cleanup. Prefix mixins like good modders.

--- Common/src/main/java/com/blamejared/initialinventory/Constants.java
+++ Common/src/main/java/com/blamejared/initialinventory/Constants.java
@@ -4,8 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Constants {
-
-	public static final String MOD_ID = "initialinventory";
-	public static final String MOD_NAME = "Initial Inventory";
-	public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    
+    public static final String MOD_ID = "initialinventory";
+    public static final String MOD_NAME = "Initial Inventory";
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    
 }
--- Fabric/src/main/java/com/blamejared/initialinventory/mixin/MixinPlayerList.java
+++ Fabric/src/main/java/com/blamejared/initialinventory/mixin/MixinPlayerList.java
@@ -2,7 +2,6 @@ package com.blamejared.initialinventory.mixin;
 
 import com.blamejared.initialinventory.InitialInventoryCommon;
 import com.faux.customentitydata.api.CustomDataHelper;
-import net.fabricmc.fabric.api.event.Event;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinPlayerList {
     
     @Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;initInventoryMenu()V"))
-    public void onLogin(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
+    public void initialInventory$onLogin(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
     
         InitialInventoryCommon.playerLogin(
                 serverPlayer,
